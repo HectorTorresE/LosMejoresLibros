2784b9926c7356385102daf367a1bec34ef934ba
HectorTorresE <hectorjte@gmail.com>
2023-04-07 19:21:39 -0400

    Add role

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # enum role: %i[free basic premium]
+  # after_initialize :set_default_role, :if => :new_record?
+  # def set_default_role
+  #   self.role ||= :free
+  # end
+  enum role: %i[client author admin]
+  after_initialize :set_default_role, :if => :new_record?
+  def set_default_role
+    self.role ||= :client
+  end
 end
