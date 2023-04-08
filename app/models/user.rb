02ee1528cc60fbc062243dd720ae1ccf099916eb
HectorTorresE <hectorjte@gmail.com>
2023-04-07 22:34:49 -0400

    Add users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,36 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  # enum role: %i[free basic premium]
-  # after_initialize :set_default_role, :if => :new_record?
-  # def set_default_role
-  #   self.role ||= :free
-  # end
+
   enum role: %i[client author admin]
-  after_initialize :set_default_role, :if => :new_record?
-  def set_default_role
+  enum membership: %i[free basic premium]
+  after_initialize :set_default_value, if: :new_record?
+  has_many :books, dependent: :destroy
+
+  def set_default_value
+    self.membership ||= :free
     self.role ||= :client
+  end
+
+  def payment
+    return unless author?
+
+    sales_to_pay = Sales.where(authorpayed: false)
+                        .where('saledate <= ?', 7.days.ago.to_date)
+                        .where(book: { user_id: id, role: 'author' })
+                        .joins(:book)
+
+    sales_to_pay.each do |sale|
+      cut = case sale.book.user.membership
+            when 'free' then 0.05
+            when 'basic' then 0.15
+            when 'premium' then 0.25
+            end
+
+      amount = sale.price * sale.copysold * cut
+      sale.book.user.balance += amount
+      sale.book.user.save!
+      sale.update(authorpayed: true, datedpayed: Time.current)
+    end
   end
 end
--- db/migrate/20230407223909_devise_create_users.rb
+++ db/migrate/20230407223909_devise_create_users.rb
@@ -4,8 +4,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,              null: false, default: ''
+      t.string :encrypted_password, null: false, default: ''
 
       ## Recoverable
       t.string   :reset_password_token
@@ -32,7 +32,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      ## costume
       t.integer :role, default: 0
+      t.integer :membership, default: 0, null: false
+      t.string :name, null: false
+      t.string :last_name, null: false
+      t.decimal :balance, default: 0.0, null: false, precision: 10, scale: 2
 
       t.timestamps null: false
     end
